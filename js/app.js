a094e50ef381f68828992b78b095373cbaf50b87
Phil Fernandez <norcal618@live.com>
2017-04-06 08:55:08 -0700

    more_api

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 window.onload = init;
 function init() {
 
-  const apiKey = 'cc909f3c6c2cd8d9f8ba69ecc11ad19c3263707a';
 
-  function callAPI(userQuery) {
-    const endpoint = 'http://api-public.guidebox.com/v2/search?sources=subscription,netflix&api_key='
-      +apiKey+'&type=movie&field=title&query='+userQuery;
-    fetch(endpoint)
-      .then(blob => blob.json())
-      .then(data => render(data));
-  }
-  
+  // function callAPI(userQuery) {
+  //   const endpoint = 'http://api-public.guidebox.com/v2/search?sources=subscription,netflix&api_key='
+  //     +apiKey+'&type=movie&field=title&query='+userQuery;
+  //   fetch(endpoint)
+  //     .then(blob => blob.json())
+  //     .then(data => render(data));
+  // }
 
+ 
 
   function getUserQuery() {
     // gets value from search box
@@ -19,9 +18,10 @@ function init() {
     callAPI(searchTerm);
   }
 
-  function render(searchResult) {
-    console.log(searchResult);
-  }
+  // function render(searchResult) {
+  //   console.log(searchResult);
+  //   populateMovies();
+  // }
 
 
   // select button and listen for click event to kick off fn that grabs user input
@@ -34,3 +34,33 @@ function init() {
 
 
 //cc909f3c6c2cd8d9f8ba69ecc11ad19c3263707a
+
+/*
+ This url loads all available show on netflix??
+
+ http://api-public.guidebox.com/v2/shows?sources=netflix&api_key=cc909f3c6c2cd8d9f8ba69ecc11ad19c3263707a
+
+
+ return all shows with limit adjusted -
+
+ http://api-public.guidebox.com/v2/shows?sources=netflix&limit=100&api_key=cc909f3c6c2cd8d9f8ba69ecc11ad19c3263707a
+
+ with offset -
+
+ http://api-public.guidebox.com/v2/shows?sources=netflix&limit=250&offset=1250&api_key=cc909f3c6c2cd8d9f8ba69ecc11ad19c3263707a
+*/
+
+/*
+ This url loads all available movies on netflix??
+
+ http://api-public.guidebox.com/v2/movies?sources=netflix&api_key=cc909f3c6c2cd8d9f8ba69ecc11ad19c3263707a
+*/
+
+/*
+ Search for specific movie only on Netflix??
+
+ http://api-public.guidebox.com/v2/search?movies?sources=netflix&api_key=cc909f3c6c2cd8d9f8ba69ecc11ad19c3263707a&type=movie&field=title&query=blow
+
+ ? in the context below just separates search attributes
+ had to add in search? before movies? and then &type=movie&field=title&query=xxxxx
+*/
